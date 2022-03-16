6aab5955ec436d1050751dd4ae5d5ebc84d65289
minhkiratsu <capminh531999@gmail.com>
2022-03-16 22:24:11 +0700

    update 2

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -2,7 +2,6 @@ import '../../css/App.css';
 import Header from '../Header/Header'
 import Left_container from '../Left_container/Left_container'
 import Right_container from '../Right_container/Right_container'
-import Test_re from '../Test_re/Test_re'
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <div className="container-lr">
           <Left_container/>
           <Right_container/>
-          <Test_re/>
         </div>
       </div>
     </div>
