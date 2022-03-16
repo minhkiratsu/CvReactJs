a18199bd9ca11150ed2e2cf3fda3a9c33ff51bc1
minhkiratsu <capminh531999@gmail.com>
2022-03-16 22:14:24 +0700

    minh update2

---
+++ src/component/Test_re/Test_re.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Test_re extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Đây là dòng test cho vui</h1>
+            </div>
+        );
+    }
+}
+
+export default Test_re;
